a8427457a9a9b6eb0f4014aea134dea9ce16552c
Thomas Rask Thomsen <thomas@semasio.com>
2021-05-16 16:24:13 +0200

    Now supporting CPU statistics.

---
+++ stimulator/src/cpu_stats.cpp
@@ -0,0 +1,28 @@
+extern "C"
+{
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "esp_system.h"
+#include "esp_log.h"
+}
+#include "cpu_stats.h"
+
+static TickType_t cpu_task_delay;
+char stats_buffer[1024];
+
+static void cpu_task( void * pvParameters )
+{
+    while(true)
+    {
+        vTaskDelay(cpu_task_delay);
+        vTaskGetRunTimeStats(stats_buffer);
+        printf("# STATS\n");
+        printf(stats_buffer);
+    }
+}
+
+void do_cpu_stats(int display_secs)
+{
+    cpu_task_delay = display_secs * 1000 / portTICK_RATE_MS;
+    xTaskCreatePinnedToCore(cpu_task, "cpu_stats_0", 2048, NULL, 1, NULL, 0);
+}
